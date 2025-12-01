5b6a849c5e3df2a8e87d6ee1edfee0a6ff93f188
tduyng <tduyng.dev@gmail.com>
2025-12-01 17:54:28 +0100

    refactor(lsp): apply vstls only for js/ts files

--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -1,4 +1,7 @@
 -- Usage: npm install -g @vtsls/language-server
+
+local ROOT_MARKERS = { "tsconfig.json", "jsconfig.json", "package.json", ".git" }
+
 return {
 	cmd = { "vtsls", "--stdio" },
 	filetypes = {
@@ -9,7 +12,28 @@ return {
 		"typescriptreact",
 		"typescript.tsx",
 	},
-	root_markers = { "tsconfig.json", "package.json", "jsconfig.json", ".git" },
+	root_dir = function(bufnr, on_dir)
+		local fname = vim.api.nvim_buf_get_name(bufnr)
+		local filetype = vim.bo[bufnr].filetype
+
+		local valid_filetypes = {
+			javascript = true,
+			javascriptreact = true,
+			["javascript.jsx"] = true,
+			typescript = true,
+			typescriptreact = true,
+			["typescript.tsx"] = true,
+		}
+
+		if not valid_filetypes[filetype] then
+			on_dir(nil)
+			return
+		end
+
+		-- Find workspace root
+		local workspace_root = vim.fs.dirname(vim.fs.find(ROOT_MARKERS, { path = fname, upward = true })[1])
+		on_dir(workspace_root or vim.fn.getcwd())
+	end,
 	settings = {
 		complete_function_calls = true,
 		vtsls = {
